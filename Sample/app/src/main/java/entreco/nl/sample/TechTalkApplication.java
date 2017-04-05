9b44339241281ee258ba2a8b25ca6cbaaa74b18a
Remco Janssen <remco.janssen@philips.com>
2017-04-05 22:02:06 +0200

    Added TechTalkApplication that provides access to ApolloClient

---
+++ Sample/app/src/main/java/entreco/nl/sample/TechTalkApplication.java
@@ -0,0 +1,28 @@
+package entreco.nl.sample;
+
+import android.app.Application;
+import com.apollographql.apollo.ApolloClient;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class TechTalkApplication extends Application {
+
+    private static final String BASE_URL = "https://api.graph.cool/simple/v1/cj15clqsb0g1n0118rncsymy7";
+    private ApolloClient apolloClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    private synchronized ApolloClient getApolloClient() {
+        if (apolloClient == null) {
+            final HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            final OkHttpClient client = new OkHttpClient.Builder().addNetworkInterceptor(loggingInterceptor).build();
+            apolloClient = ApolloClient.builder().serverUrl(BASE_URL).okHttpClient(client).build();
+        }
+
+        return apolloClient;
+    }
+}
